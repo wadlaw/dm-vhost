a8559640d2d03420ec455f17b1fb837de36b4c5c
Tom <33520715+wadlaw@users.noreply.github.com>
2021-06-01 11:33:52 +0100

    get all processes running under vhost

--- server.js
+++ server.js
@@ -6,19 +6,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 4000
 
 const apiApp = require('../api/server').app;
+const webApp = require('../webapp/server').app;
 
 //app.use(Incoming);
 app.use(vhost('api.*', apiApp));
 //.use(vhost('sync.mysite.com', require('/path/to/sync').app))
-app.use(vhost('carbon.*', function handle (req, res, next) {
-    // for match of "foo.bar.example.com:8080" against "*.*.example.com":
-    console.log(req.vhost.host) // => 'foo.bar.example.com:8080'
-    console.log(req.vhost.hostname) // => 'foo.bar.example.com'
-    console.log(req.vhost.length) // => 2
-    console.log(req.vhost[0]) // => 'foo'
-    console.log(req.vhost[1]) // => 'bar'
-    res.send(`Hello from ${req.vhost.hostname}`);
-  }))
+app.use(vhost('carbon.*', webApp))
 app.get('/ping', (req, res) => {
     res.send("Hello, world!");
 });
